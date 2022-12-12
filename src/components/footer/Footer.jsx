a6adb72e4f7d8cd12db14312d3b9b789805b8443
Manel <manelhammouche3@gmail.com>
2022-12-12 19:15:13 +0100

    Add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="w-full absolute bottom-0 flex justify-center py-4 bg-gray-700 text-white">
+    <div className="font-bold text-xl">Discogs</div>
+    <ul className="flex ml-6 items-center">
+      <li className="mx-2">Cookie Policy</li>
+      <li className="mx-2">Cookie Policy</li>
+      <li className="mx-2">Cookie Policy</li>
+      <li className="mx-2">Cookie Policy</li>
+      <li className="mx-2">Cookie Policy</li>
+    </ul>
+  </div>
+);
+
+export default Footer;
