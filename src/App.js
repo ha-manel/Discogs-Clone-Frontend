986e90642708c89d8f7f67d03948299e62870d9d
Manel <manelhammouche3@gmail.com>
2022-12-12 19:13:49 +0100

    Add api call and contexts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Layout from './config/layout/Layout';
+import RecordsContext from './context/recordsContext';
+import StatsContext from './context/statsContext';
 
-function App() {
+const App = () => {
+  const [records, setRecords] = useState([]);
+  const [stats, setStats] = useState({});
+  const [result, setResult] = useState([]);
+
+  const fetchRecords = async () => {
+    await axios.get('http://localhost:3000/api/v1/records').then((response) => {
+      setResult(response.data.records);
+      setRecords(response.data.records);
+      setStats(response.data.stats);
+    });
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, []);
+
   return (
-    <div className="App">
-    </div>
+    <RecordsContext.Provider
+      value={{ APIresult: [result, setResult], displayRecords: [records, setRecords] }}
+    >
+      <StatsContext.Provider value={[stats, setStats]}>
+        <Layout />
+      </StatsContext.Provider>
+    </RecordsContext.Provider>
   );
-}
-
+};
 export default App;
