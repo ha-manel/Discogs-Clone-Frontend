0a279f7ed6b47943dcf52399b4493946221a88ad
Manel <manelhammouche3@gmail.com>
2022-12-12 22:26:19 +0100

    Update api call

--- src/components/createForm/CreateForm.jsx
+++ src/components/createForm/CreateForm.jsx
@@ -13,18 +13,22 @@ const CreateForm = () => {
   const [genre, setGenre] = useState('');
   const [picture, setPicture] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const record = {
-      title,
-      artist,
-      year,
-      genre,
-      image_data: picture,
-    };
-    axios.post('http://localhost:3000/api/v1/record', { record }).then((response) => {
+    const record = new FormData();
+    record.append('title', title);
+    record.append('artist', artist);
+    record.append('year', year);
+    record.append('genre', genre);
+    record.append('image_data', picture);
+    await axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/v1/record',
+      data: record,
+    }).then((response) => {
       if (response.data.status === 'created') {
         const newRecords = [...result, response.data.record];
+        console.log(response.data.record);
         setResult(newRecords);
         setRecords(newRecords);
       }
@@ -40,7 +44,7 @@ const CreateForm = () => {
         <input type="text" placeholder="Artist" className="my-2 py-2 px-2 border border-gray-400 rounded" value={artist} onChange={(e) => setArtist(e.target.value)} />
         <input type="number" placeholder="Year" className="my-2 py-2 px-2 border border-gray-400 rounded" value={year} onChange={(e) => setYear(e.target.value)} />
         <input type="text" placeholder="Genre" className="my-2 py-2 px-2 border border-gray-400 rounded" value={genre} onChange={(e) => setGenre(e.target.value)} />
-        <input type="text" placeholder="Album cover" className="my-2 py-2 px-2 border border-gray-400 rounded" value={picture} onChange={(e) => setPicture(e.target.value)} />
+        <input type="file" accept="image/*" multiple={false} className="my-2 py-2 px-2 border border-gray-400 rounded" onChange={(e) => setPicture(e.target.files[0])} />
         <input type="submit" value="Create" className="bg-gray-500 w-fit text-white py-2 px-3 rounded cursor-pointer mt-4" />
       </form>
     </div>
