9574f3a03541efe2538d3dc42161210deb2bcb68
Manel <manelhammouche3@gmail.com>
2022-12-15 15:58:46 +0100

    Add production url in API calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import './App.css';
 import Layout from './config/layout/Layout';
 import RecordsContext from './context/recordsContext';
 import StatsContext from './context/statsContext';
@@ -11,7 +10,7 @@ const App = () => {
   const [result, setResult] = useState([]);
 
   const fetchRecords = async () => {
-    await axios.get('http://localhost:3000/api/v1/records').then((response) => {
+    await axios.get('https://discogs-clone.onrender.com/api/v1/records').then((response) => {
       setResult(response.data.records);
       setRecords(response.data.records);
       setStats(response.data.stats);
